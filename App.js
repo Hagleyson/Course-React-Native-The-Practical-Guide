397c3092217fca636f05138260904670900a11af
hagleyson <hagleyson@gmail.com>
2022-02-03 14:54:09 -0300

    Working with State & Events

--- App.js
+++ App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { Button, StyleSheet, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [couseGoal, setCouseGoal] = useState("");
+  console.log(couseGoal);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoal(enteredText);
+  }
+
+  const addGoalHanlder = () => {
+    setCouseGoal((currentGoals) => [enteredGoal, ...currentGoals]);
+    setEnteredGoal("");
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder="Course Goal" style={styles.input} />
-        <Button style={{ width: "20%" }} title="add" />
+        <TextInput
+          placeholder="Course Goal"
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button style={{ width: "20%" }} title="add" onPress={addGoalHanlder} />
       </View>
       <View></View>
     </View>
