07d7a2c8026cec6e948058b9b9734969dee60d29
hagleyson <hagleyson@gmail.com>
2022-02-05 23:37:22 -0300

    Styling Images

--- project 02/screens/GameOverScreen.js
+++ project 02/screens/GameOverScreen.js
@@ -9,7 +9,13 @@ const GameOverScreen = (props) => {
     <View style={styles.screen}>
       <TitleText>The Game is Over!</TitleText>
       <BodyText>Number of rounds: {props.roundsNumbers}</BodyText>
-      <Image source={backgroundImage} />
+      <View style={styles.imageContainer}>
+        <Image
+          source={require("../assets/success.png")}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </View>
       <BodyText>Number was: {props.userNumber}</BodyText>
       <Button
         title="NEW GAME"
@@ -22,8 +28,24 @@ const GameOverScreen = (props) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    width: "100%",
     justifyContent: "center",
     alignItems: "center",
+  },
+  imageContainer: {
+    width: 300,
+    height: 300,
+    marginVertical: 10,
+    borderRadius: 150,
+    borderWidth: 3,
+    borderColor: "black",
+    justifyContent: "center",
+    alignItems: "center",
+    overflow: "hidden",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
   },
 });
 export default GameOverScreen;
