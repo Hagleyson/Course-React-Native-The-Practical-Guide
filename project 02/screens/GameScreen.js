dedeea7f5492181e30c0041cfbe5dd152e143fa1
hagleyson <hagleyson@gmail.com>
2022-02-07 13:25:45 -0300

    Using Dimensions in if Checks

--- project 02/screens/GameScreen.js
+++ project 02/screens/GameScreen.js
@@ -6,6 +6,7 @@ import {
   Alert,
   ScrollView,
   FlatList,
+  Dimensions,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
@@ -112,17 +113,16 @@ const styles = StyleSheet.create({
   buttonContainer: {
     flexDirection: "row",
     justifyContent: "space-around",
-    marginTop: 20,
+    marginTop: Dimensions.get("window").height > 600 ? 20 : 10,
     width: 400,
     maxWidth: "90%",
   },
   listContainer: {
     flex: 1,
-    width: "60%",
+    width: Dimensions.get("window").width > 350 ? "60%" : "80%",
   },
   list: {
     flexGrow: 1,
-    // alignItems: 'center',
     justifyContent: "flex-end",
   },
   listItem: {
