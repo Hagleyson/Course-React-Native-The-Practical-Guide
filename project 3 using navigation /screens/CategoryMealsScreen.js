8b8dd773a73ecabdebecf01d5925e141933b4e94
hagleyson <hagleyson@gmail.com>
2022-02-08 14:28:52 -0300

    Loading Meals for Categories

--- project 3 using navigation /screens/CategoryMealsScreen.js
+++ project 3 using navigation /screens/CategoryMealsScreen.js
@@ -1,25 +1,35 @@
 import React from "react";
 
-import { View, Text, StyleSheet, Button } from "react-native";
-import { CATEGORIES } from "../data/dummy-data";
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+import { CATEGORIES, MEALS } from "../data/dummy-data";
 
 const CategoryMeal = (props) => {
   const catId = props.navigation.getParam("categoryId");
   const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
 
+  const displayedMeals = MEALS.filter(
+    (meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
+
+  const renderMealItem = (itemData) => {
+    return (
+      <View>
+        <Text>{itemData.item.title}</Text>
+      </View>
+    );
+  };
   return (
     <View style={styles.screen}>
-      <Text>The Category Meal Screen!</Text>
+      <FlatList data={displayedMeals} renderItem={renderMealItem} />
+      {/* <Text>The Category Meal Screen!</Text>
       <Text>{selectedCategory.title}</Text>
       <Button
         title="GO to Details"
         onPress={() => {
-          /*props.navigation.push("MealDetail")
-           *
-           */
+          props.navigation.push("MealDetail")          
           props.navigation.navigate("MealDetail");
         }}
-      />
+      /> */}
       {/* <Button
         title="GO back"
         onPress={() => {
