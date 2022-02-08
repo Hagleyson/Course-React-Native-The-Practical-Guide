7055a49ed02bdd826495c3c32f093c5199d951ec
hagleyson <hagleyson@gmail.com>
2022-02-08 19:15:00 -0300

    Setting Icons and Configuring Tabs

--- project 3 using navigation /navigation/MealsNavigator.js
+++ project 3 using navigation /navigation/MealsNavigator.js
@@ -3,6 +3,7 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import { createBottomTabNavigator } from "react-navigation-tabs";
 
+import { Ionicons } from "@expo/vector-icons";
 import Colors from "../constants/Colors";
 
 import CategoriesScreen from "../screens/CategoriesScreen";
@@ -31,8 +32,39 @@ const MealsNavigator = createStackNavigator(
   }
 );
 
-const MealsFavTabNavigator = createBottomTabNavigator({
-  Meals: MealsNavigator,
-  FavoritesScreen: FavoritesScreen,
-});
+const MealsFavTabNavigator = createBottomTabNavigator(
+  {
+    Meals: {
+      screen: MealsNavigator,
+      navigationOptions: {
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons
+              name="ios-restaurant"
+              size={25}
+              color={tabInfo.tintColor}
+            />
+          );
+        },
+      },
+    },
+    FavoritesScreen: {
+      screen: FavoritesScreen,
+
+      navigationOptions: {
+        tabBarLabel: "Favorites ",
+        tabBarIcon: (tabInfo) => {
+          return (
+            <Ionicons name="ios-star" size={25} color={tabInfo.tintColor} />
+          );
+        },
+      },
+    },
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: Colors.accentColor,
+    },
+  }
+);
 export default createAppContainer(MealsFavTabNavigator);
