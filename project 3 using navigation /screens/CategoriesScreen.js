a8447aaba0520bc9fbacc16bc36f7a75f55fe168
hagleyson <hagleyson@gmail.com>
2022-02-08 11:24:41 -0300

    Outputting a Grid of Categories

--- project 3 using navigation /screens/CategoriesScreen.js
+++ project 3 using navigation /screens/CategoriesScreen.js
@@ -1,22 +1,30 @@
 import React from "react";
 
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
+
+const renderGridItem = (itemData) => {
+  return (
+    <View style={styles.gridItem}>
+      <Text>{itemData.item.title}</Text>
+    </View>
+  );
+};
 
 const CategoriesScreen = (props) => {
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen!</Text>
-      <Button
+    <FlatList data={CATEGORIES} renderItem={renderGridItem} numColumns={2} />
+
+    /*  <Button
         title="Go to Meals!"
         onPress={() => {
-          /*
-          props.navigation.replace("CategoryMeals");
-          muito usado para  login
-          */
+          
+          //props.navigation.replace("CategoryMeals");
+          //muito usado para  login
+          
           props.navigation.navigate("CategoryMeals");
         }}
-      />
-    </View>
+      />*/
   );
 };
 
@@ -25,6 +33,11 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignContent: "center",
+  },
+  gridItem: {
+    flex: 1,
+    margin: 15,
+    height: 150,
   },
 });
 
