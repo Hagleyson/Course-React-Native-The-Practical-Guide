d4bc1fc538e9ddcebfb61cb8ca86280ed2f9c40d
hagleyson <hagleyson@gmail.com>
2022-02-08 12:36:05 -0300

    Default Navigation Options & Config

--- project 3 using navigation /navigation/MealsNavigator.js
+++ project 3 using navigation /navigation/MealsNavigator.js
@@ -1,13 +1,30 @@
+import { Platform } from "react-native";
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
+import Colors from "../constants/Colors";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMeal from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetaisScreen";
 
-const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  CategoryMeals: CategoryMeal,
-  MealDetail: MealDetailScreen,
-});
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: CategoriesScreen,
+    CategoryMeals: {
+      screen: CategoryMeal,
+    },
+    MealDetail: MealDetailScreen,
+  },
+  {
+    // mode: "card",
+    // initialRouteName: "MealDetail",
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+      },
+      headerTintColor:
+        Platform.OS === "android" ? "white" : Colors.primaryColor,
+    },
+  }
+);
 
 export default createAppContainer(MealsNavigator);
