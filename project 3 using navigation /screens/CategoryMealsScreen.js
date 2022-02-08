3f65b622b91b9f72ca028fb9fd1e0f47f6e36ebc
hagleyson <hagleyson@gmail.com>
2022-02-08 12:17:37 -0300

    Setting Dynamic Navigation Options

--- project 3 using navigation /screens/CategoryMealsScreen.js
+++ project 3 using navigation /screens/CategoryMealsScreen.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, Button, Platform } from "react-native";
 import { CATEGORIES } from "../data/dummy-data";
 
 const CategoryMeal = (props) => {
   const catId = props.navigation.getParam("categoryId");
   const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
-  console.log(selectedCategory);
+
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
@@ -30,7 +30,17 @@ const CategoryMeal = (props) => {
     </View>
   );
 };
-
+CategoryMeal.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+  return {
+    headerTitle: selectedCategory.title,
+    headerStyle: {
+      backgroundColor: Platform.OS === "android" ? Colors.primaryColor : "",
+    },
+    headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor,
+  };
+};
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
