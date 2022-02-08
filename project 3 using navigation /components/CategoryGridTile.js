6f28ac08d879ec0cf0ffae2418f54780a9736e7d
hagleyson <hagleyson@gmail.com>
2022-02-08 17:11:39 -0300

    Fixing the Shadows

--- project 3 using navigation /components/CategoryGridTile.js
+++ project 3 using navigation /components/CategoryGridTile.js
@@ -23,25 +23,30 @@ const CategoryGridTile = (props) => {
   );
 };
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 10,
-    borderRadius: 10,
-    shadowColor: "black",
-    shadowOpacity: 0.26,
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 10,
-    elevation: 3,
-    justifyContent: "flex-end",
-    alignItems: "flex-end",
-  },
   gridItem: {
     flex: 1,
     margin: 15,
     height: 150,
     borderRadius: 10,
-    overflow: "hidden",
+    overflow:
+      Platform.OS === "android" && Platform.Version >= 21
+        ? "hidden"
+        : "visible",
+    elevation: 5,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 10,
   },
+  container: {
+    flex: 1,
+    padding: 10,
+    borderRadius: 10,
+
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+  },
+
   title: {
     fontFamily: "open-sans-bold",
     fontSize: 15,
