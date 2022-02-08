dfc8065ef307c0bb233224a4a963334862e3161e
hagleyson <hagleyson@gmail.com>
2022-02-08 12:09:01 -0300

    Passing & Reading Params Upon Navigation

--- project 3 using navigation /screens/CategoryMealsScreen.js
+++ project 3 using navigation /screens/CategoryMealsScreen.js
@@ -1,11 +1,16 @@
 import React from "react";
 
 import { View, Text, StyleSheet, Button } from "react-native";
+import { CATEGORIES } from "../data/dummy-data";
 
 const CategoryMeal = (props) => {
+  const catId = props.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find((cat) => cat.id === catId);
+  console.log(selectedCategory);
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen!</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button
         title="GO to Details"
         onPress={() => {
@@ -15,13 +20,13 @@ const CategoryMeal = (props) => {
           props.navigation.navigate("MealDetail");
         }}
       />
-      <Button
+      {/* <Button
         title="GO back"
         onPress={() => {
           // props.navigation.goBack();
           props.navigation.pop();
         }}
-      />
+      /> */}
     </View>
   );
 };
@@ -30,7 +35,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: "center",
-    alignContent: "center",
+    alignItems: "center",
   },
 });
 
--- project 3 using navigation /screens/MealDetaisScreen.js
+++ project 3 using navigation /screens/MealDetaisScreen.js
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     justifyContent: "center",
-    alignContent: "center",
+    alignItems: "center",
   },
 });
 
