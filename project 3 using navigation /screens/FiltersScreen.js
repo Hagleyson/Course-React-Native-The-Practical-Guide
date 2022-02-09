918eeb0ac0d12b3fa21a16f96f4f6023d26fdff6
hagleyson <hagleyson@gmail.com>
2022-02-09 18:26:03 -0300

    Time for the Filters Screen Content!

--- project 3 using navigation /screens/FiltersScreen.js
+++ project 3 using navigation /screens/FiltersScreen.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Switch, Platform } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import HeaderButton from "../components/HeaderButton";
+import Colors from "../constants/Colors";
+
+const FilterSwitch = (props) => {
+  return (
+    <View style={styles.filterItem}>
+      <Text>{props.title}</Text>
+      <Switch
+        trackColor={{ false: Colors.accentColor, true: Colors.primaryColor }}
+        thumbColor={Platform.OS === "android" ? Colors.primaryColor : ""}
+        value={props.value}
+        onValueChange={(newValue) => props.onValueChange(newValue)}
+      />
+    </View>
+  );
+};
+
 const FiltersScreen = (props) => {
+  const [isGlutenFree, setIsGlutenFree] = useState(false);
+  const [isLactoseFree, setIsLactoseFree] = useState(false);
+  const [isVegan, setIsVegan] = useState(false);
+  const [isVegetarian, setIsVegetarian] = useState(false);
+
   return (
     <View style={styles.screen}>
-      <Text>The Filters Screen !</Text>
+      <Text style={styles.title}>Available Filters / Restrictions</Text>
+      <View style={styles.filterContainer}>
+        <FilterSwitch
+          title="Gluten-free"
+          value={isGlutenFree}
+          onValueChange={setIsGlutenFree}
+        />
+        <FilterSwitch
+          title="Lactose-free"
+          value={isLactoseFree}
+          onValueChange={setIsLactoseFree}
+        />
+        <FilterSwitch
+          title="Vegan"
+          value={isVegan}
+          onValueChange={setIsVegan}
+        />
+        <FilterSwitch
+          title="Vegetarian"
+          value={isVegetarian}
+          onValueChange={setIsVegetarian}
+        />
+      </View>
     </View>
   );
 };
@@ -30,8 +73,25 @@ FiltersScreen.navigationOptions = (navData) => {
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: "center",
     alignItems: "center",
+  },
+  title: {
+    fontFamily: "open-sans-bold",
+    fontSize: 22,
+    margin: 20,
+    textAlign: "center",
+  },
+  filterContainer: {
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "80%",
+  },
+  filterItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    width: "100%",
   },
 });
 
