6f79fb1e4f6401e7d6b5ab4507656d5f92abf03a
hagleyson <hagleyson@gmail.com>
2022-02-10 12:49:17 -0300

    The Products Overview Screen

--- project 4 practice-shop/App.js
+++ project 4 practice-shop/App.js
@@ -1,5 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+
+import ProductsReducer from "./store/reducers/products";
 
 export default function App() {
   return (
@@ -13,8 +18,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
