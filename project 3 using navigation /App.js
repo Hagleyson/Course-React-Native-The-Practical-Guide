35b0f3357a90736189a96541e7c8d77ad183d410
hagleyson <hagleyson@gmail.com>
2022-02-09 22:57:40 -0300

    Redux & Store Setup

--- project 3 using navigation /App.js
+++ project 3 using navigation /App.js
@@ -5,6 +5,15 @@ import * as Font from "expo-font";
 import AppLoaging from "expo-app-loading";
 import { useState } from "react";
 
+import { createStore, combineReducers } from "redux";
+import mealsReducer from "./store/reducers/meals";
+import { Provider } from "react-redux";
+
+const rootreducer = combineReducers({
+  meals: mealsReducer,
+});
+const store = createStore(rootreducer);
+
 const fetchFonts = () => {
   return Font.loadAsync({
     "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
@@ -24,5 +33,9 @@ export default function App() {
       />
     );
   }
-  return <MealsNavigator />;
+  return (
+    <Provider store={store}>
+      <MealsNavigator />
+    </Provider>
+  );
 }
