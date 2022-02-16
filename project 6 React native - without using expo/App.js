e60be71b513fa3f7d56c43bbbda5da1058a42aa6
hagleyson <hagleyson@gmail.com>
2022-02-16 12:06:12 -0300

    finished module 13

---
+++ project 6 React native - without using expo/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import {StyleSheet, View, Button} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+const App = () => {
+  const pickImage = () => {
+    const options = {
+      title: 'Select Avatar',
+      customButtons: [{name: 'fb', title: 'Choose Photo from Facebook'}],
+      storageOptions: [
+        {
+          skipBackup: true,
+          path: 'images',
+        },
+      ],
+    };
+    ImagePicker.showImagePicker(options, response => {
+      console.log('Response = ', response);
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button', response.customButton);
+      } else {
+        console.log(response.uri);
+      }
+    });
+  };
+  return (
+    <View>
+      <Button title="Enviar" onPress={pickImage} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
